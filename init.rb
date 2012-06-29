25d7dc995877cfb2e64f046c1d9837f83fce5a38
Sawan Shah <sawan1990@gmail.com>
2012-06-29 16:28:25 +0530

    made it public

--- init.rb
+++ init.rb
@@ -5,8 +5,8 @@ Redmine::Plugin.register :redmine_sub_project_stats do
   author 'Sawan Shah'
   description 'This is a plugin for Redmine'
   version '0.0.1'
-  project_module :polls do
-    permission :get_stats, :project_stats => :get_project_stats    
+  project_module :get_subproject_stats do
+    permission :get_stats, :project_stats => :get_project_stats   , :public => true
   end
   menu :project_menu, :project_stats, { :controller => 'project_stats', :action => 'get_project_stats' }, :caption => 'Project Stats',  :after => :settings, :param => :project_id
  end

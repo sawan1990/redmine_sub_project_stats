c7f628707487d1ec164d3fe3b547c048fbb38471
Sawan Shah <sawan1990@gmail.com>
2012-06-29 16:32:02 +0530

    added permission

--- init.rb
+++ init.rb
@@ -6,7 +6,8 @@ Redmine::Plugin.register :redmine_sub_project_stats do
   description 'This is a plugin for Redmine'
   version '0.0.1'
   project_module :get_subproject_stats do
-    permission :get_stats, :project_stats => :get_project_stats   , :public => true
+    permission :get_subproject_stats, :project_stats => :get_project_stats
   end
+  permission :view_subproject_stats, :project_stats => :get_project_stats
   menu :project_menu, :project_stats, { :controller => 'project_stats', :action => 'get_project_stats' }, :caption => 'Project Stats',  :after => :settings, :param => :project_id
  end

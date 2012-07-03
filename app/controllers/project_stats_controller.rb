eb15def3a45a9d77e31cdb187351ed5125bbc0ec
Sawan Shah <sawan1990@gmail.com>
2012-07-03 11:02:47 +0530

    solved date problem

--- app/controllers/project_stats_controller.rb
+++ app/controllers/project_stats_controller.rb
@@ -4,8 +4,8 @@ class ProjectStatsController < ApplicationController
   def get_project_stats
     @parent_project = Project.find(params[:project_id])
     @project = @parent_project
-    @end_date = DateTime.civil(params[:end_date][:year].to_i, params[:end_date][:month].to_i, params[:end_date][:day].to_i , params[:end_date][:hour].to_i , params[:end_date][:minute].to_i ).to_time rescue nil || Time.now.to_datetime
-    @start_date = DateTime.civil(params[:start_date][:year].to_i, params[:start_date][:month].to_i, params[:start_date][:day].to_i , params[:start_date][:hour].to_i , params[:start_date][:minute].to_i ).to_time rescue nil || (Time.now - 7.days).to_datetime
+    @end_date = DateTime.civil(params[:end_date][:year].to_i, params[:end_date][:month].to_i, params[:end_date][:day].to_i , params[:end_date][:hour].to_i , params[:end_date][:minute].to_i ) rescue nil || Time.now.to_datetime
+    @start_date = DateTime.civil(params[:start_date][:year].to_i, params[:start_date][:month].to_i, params[:start_date][:day].to_i , params[:start_date][:hour].to_i , params[:start_date][:minute].to_i ) rescue nil || (Time.now - 7.days).to_datetime
     @projects = @parent_project.children
     @projects.each do |project|
       @projects = @projects + project.children

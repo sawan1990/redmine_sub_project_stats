7853f58705cb3b9f64c70c2eed5d548df50d955d
redmine <redmine@raman.(none)>
2012-07-03 13:35:57 +0530

    minor

--- init.rb
+++ init.rb
@@ -8,5 +8,6 @@ Redmine::Plugin.register :redmine_sub_project_stats do
   project_module :get_subproject_stats do
     permission :get_subproject_stats, :project_stats => :get_project_stats
   end
-  menu :project_menu, :project_stats, { :controller => 'project_stats', :action => 'get_project_stats' }, :caption => 'Project Stats',  :after => :settings, :param => :project_id
+permission :view_subproject_stats, :project_stats => :get_project_stats
+  menu :project_menu, :sub_project_stats, { :controller => 'project_stats', :action => 'get_project_stats' }, :caption => 'Sub Project Stats',  :after => :settings, :param => :project_id
  end

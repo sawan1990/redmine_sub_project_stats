c97421d38f7bad444167d8024b1226b2bd02cb91
Sawan Shah <sawan1990@gmail.com>
2012-07-12 14:36:20 +0530

    only if project has sub project

--- app/controllers/project_stats_controller.rb
+++ app/controllers/project_stats_controller.rb
@@ -8,7 +8,7 @@ class ProjectStatsController < ApplicationController
     @start_date = DateTime.civil(params[:start_date][:year].to_i, params[:start_date][:month].to_i, params[:start_date][:day].to_i , params[:start_date][:hour].to_i , params[:start_date][:minute].to_i ) rescue nil || (Time.now - 7.days).to_datetime
     @projects = @parent_project.children
     @projects.each do |project|
-      @projects = @projects + project.children
+      @projects = @projects + (project.children rescue nil)
     end
     
   end
--- init.rb
+++ init.rb
@@ -9,5 +9,5 @@ Redmine::Plugin.register :redmine_sub_project_stats do
     permission :get_subproject_stats, :project_stats => :get_project_stats
   end
 permission :view_subproject_stats, :project_stats => :get_project_stats
-  menu :project_menu, :sub_project_stats, { :controller => 'project_stats', :action => 'get_project_stats' }, :caption => 'Sub Project Stats',  :after => :settings, :param => :project_id
+  menu :project_menu, :sub_project_stats, { :controller => 'project_stats', :action => 'get_project_stats' }, :caption => 'Sub Project Stats',  :before => :settings, :param => :project_id, :if =>Proc.new {|p|  p.children.any?}
  end
